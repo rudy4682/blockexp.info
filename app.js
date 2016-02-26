2fa1c98f3a197f0d94f7bafbfb8b9708bfa40349
Rudy4682 <radams4682@gmail.com>
2016-02-26 03:55:01 -0600

    Added GETCOINSUPPLY to retreive from getsupply api call

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var app = express();
 bitcoinapi.setWalletDetails(settings.wallet);
 if (settings.heavy != true) {
   bitcoinapi.setAccess('only', ['getinfo', 'getnetworkhashps', 'getmininginfo', 'getdifficulty', 'getconnectioncount', 
-    'getblockcount', 'getblockhash', 'getblock', 'getrawtransaction', 'getpeerinfo']);
+    'getblockcount', 'getblockhash', 'getblock', 'getrawtransaction', 'getpeerinfo', 'getcoinsupply']);
 } else {
   // enable additional heavy api calls
   /*
